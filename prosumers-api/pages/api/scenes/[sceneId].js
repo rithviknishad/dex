0fe6e4ad121aefc9a8f25e62342d9622d34e762b
rithviknishad <rithvikn2001@gmail.com>
2023-02-17 15:04:53 +0530

    deouple api

---
+++ prosumers-api/pages/api/scenes/[sceneId].js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+export default async function handler(req, res) {
+  const { MONGO_URL } = process.env;
+  const { sceneId } = req.query;
+
+  if (!MONGO_URL) {
+    res.status(500).json({ detail: "MONGO_URL not set" });
+    return;
+  }
+
+  const connection = await mongoose
+    .connect(MONGO_URL)
+    .catch((err) => res.status(500).json({ detail: err.message }));
+
+  if (!connection) {
+    return;
+  }
+
+  const collection = connection.connection.db.collection("scenes");
+  const _id = new mongoose.Types.ObjectId(sceneId);
+
+  if (req.method === "GET") {
+    const scene = await collection.findOne({ _id });
+
+    if (!scene) {
+      res.status(404).json({ detail: "Not found" });
+      return;
+    }
+
+    res.status(200).json({ id: scene._id.toString(), scene });
+    return;
+  }
+
+  if (req.method === "PUT") {
+    const scene = req.body;
+
+    const now = new Date().toISOString();
+
+    const result = await collection.updateOne(
+      { _id },
+      { $set: { ...scene, updated_at: now } }
+    );
+
+    if (result.modifiedCount === 0) {
+      res.status(404).json({ detail: "Not found" });
+      return;
+    }
+
+    res.status(200).json({ id: sceneId });
+    return;
+  }
+
+  if (req.method === "DELETE") {
+    const result = await collection.deleteOne({ _id });
+
+    if (result.deletedCount === 0) {
+      res.status(404).json({ detail: "Not found" });
+      return;
+    }
+
+    res.status(200).json({ id: sceneId });
+    return;
+  }
+}
