0367a1f9caf6f0d7bb68ecb33e3a748a97ca9c35
rithviknishad <rithvikn2001@gmail.com>
2023-03-11 23:33:25 +0530

    utility hook: `useFormState`

---
+++ prosumers/utils/useFormState.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
+export default function useFormState<T extends object>(
+  initialState: T
+): [T, <K extends keyof T>(key: K, value: T[K]) => void] {
+  const [state, setState] = useState<T>(initialState);
+  const setValueOf = <K extends keyof T>(key: K, value: typeof state[K]) => {
+    return setState((state) => ({ ...state, [key]: value }));
+  };
+
+  return [state, setValueOf];
+}
