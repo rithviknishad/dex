02067ea8fb88f7ae4ec6a1d19fba51f5671e3bb4
rithviknishad <rithvikn2001@gmail.com>
2023-03-11 08:42:01 +0530

    hide ScenesSidebar if editing scene

--- prosumers/app/dashboard/scenes/layout.tsx
+++ prosumers/app/dashboard/scenes/layout.tsx
@@ -4,15 +4,22 @@ import StreamProvider from "@/components/providers/StreamProvider";
 import FirebaseContext from "@/contexts/FirebaseContext";
 import ScenesContext from "@/contexts/ScenesContext";
 import { ref } from "firebase/database";
+import { usePathname } from "next/navigation";
 import { useContext } from "react";
 import ScenesSidebar from "./ScenesSidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { db } = useContext(FirebaseContext);
+
+  const pathSegments = usePathname().split("/");
+  const showSidebar = !(
+    pathSegments[2] === "scenes" && pathSegments[4] === "edit"
+  );
+
   return (
     <StreamProvider context={ScenesContext} source={ref(db, "scenes")}>
       <div className="flex flex-row h-full w-full">
-        <ScenesSidebar />
+        {showSidebar && <ScenesSidebar />}
         <div className="overflow-auto h-full w-full">{children}</div>
       </div>
     </StreamProvider>
