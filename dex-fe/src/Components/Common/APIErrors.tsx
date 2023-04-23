639d8bc79a000e7a0776be562e0598d5be562dcf
rithviknishad <rithvikn2001@gmail.com>
2023-04-23 19:55:18 +0530

    API errors support asRecord

--- dex-fe/src/Components/Common/APIErrors.tsx
+++ dex-fe/src/Components/Common/APIErrors.tsx
@@ -2,18 +2,27 @@ import { classNames } from "../../utils/classNames";
 
 const ERR = { err: "Unknown error" };
 
-export default function APIErrors({ error }: { error: any }) {
+export default function APIErrors({
+  error,
+  asRecord,
+}: {
+  error: any;
+  asRecord?: boolean;
+}) {
   const errors = Object.entries(error.response?.data || ERR);
 
   return (
     <ul
       className={classNames(
         "block sm:inline",
-        errors.length > 1 && "space-y-2 list-decimal ml-6"
+        !asRecord && errors.length > 1 && "space-y-2 list-decimal ml-6"
       )}
     >
       {errors.map(([key, value]) => (
-        <li key={key}>{value as any}</li>
+        <li key={key}>
+          {asRecord ? `${key}: ` : ""}
+          {value as any}
+        </li>
       ))}
     </ul>
   );
